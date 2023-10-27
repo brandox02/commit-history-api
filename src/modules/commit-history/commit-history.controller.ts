0836665d14a5ab69054863bbb682d06ff65b5e41
Brandon Fernandez <brandox02.dev@gmail.com>
2023-10-27 11:31:25 -0400

    unussed endpoint from commit-history were removed, github commit api response was typified

--- src/modules/commit-history/commit-history.controller.ts
+++ src/modules/commit-history/commit-history.controller.ts
@@ -5,30 +5,13 @@ import { UpdateCommitHistoryDto } from './dto/update-commit-history.dto';
 
 @Controller('commit-history')
 export class CommitHistoryController {
-  constructor(private readonly commitHistoryService: CommitHistoryService) {}
+  constructor(private readonly commitHistoryService: CommitHistoryService) { }
 
-  @Post()
-  create(@Body() createCommitHistoryDto: CreateCommitHistoryDto) {
-    return this.commitHistoryService.create(createCommitHistoryDto);
-  }
 
   @Get()
   findAll() {
-    return this.commitHistoryService.findAll();
+    return this.commitHistoryService.findAll('', '');
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.commitHistoryService.findOne(+id);
-  }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommitHistoryDto: UpdateCommitHistoryDto) {
-    return this.commitHistoryService.update(+id, updateCommitHistoryDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.commitHistoryService.remove(+id);
-  }
 }
--- src/modules/commit-history/commit-history.service.ts
+++ src/modules/commit-history/commit-history.service.ts
@@ -1,30 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommitHistoryDto } from './dto/create-commit-history.dto';
 import { UpdateCommitHistoryDto } from './dto/update-commit-history.dto';
+import { Commit } from './types';
 
 @Injectable()
 export class CommitHistoryService {
-  create(createCommitHistoryDto: CreateCommitHistoryDto) {
-    return 'This action adds a new commitHistory';
-  }
-
-  async findAll(username: string, repoName: string) {
-
+  async findAll(username: string, repoName: string): Promise<Commit[]> {
 
     const response = await fetch(`https://api.github.com/repos/${username}/${repoName}/commits`);
     const commits = await response.json();
     return commits;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} commitHistory`;
-  }
-
-  update(id: number, updateCommitHistoryDto: UpdateCommitHistoryDto) {
-    return `This action updates a #${id} commitHistory`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} commitHistory`;
-  }
 }
